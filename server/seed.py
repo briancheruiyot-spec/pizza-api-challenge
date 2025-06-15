cd5ea2ebddd48d555585d01a56525f5d50cf5711
briancheruiyot-spec <briancheruiyot6@gmail.com>
2025-06-15 22:58:14 +0300

    Updated seed.py

---
+++ server/seed.py
@@ -0,0 +1,23 @@
+from . import create_app
+from .models import db, Restaurant, Pizza, RestaurantPizza
+
+app = create_app()
+
+with app.app_context():
+  db.drop_all()
+  db.create_all()
+
+  r1 = Restaurant(name="Pizza Palace", address="123 Main St")
+  r2 = Restaurant(name="Mario's Pizzeria", address="456 Oak Ave")
+
+  p1 = Pizza(name="Margherita", ingredients="Tomato, Mozzarella, Basil")
+  p2 = Pizza(name="Pepperoni", ingredients="Tomato, Mozzarella, Pepperoni")
+
+  db.session.add_all([r1, r2, p1, p2])
+  db.session.commit()
+
+  rp1 = RestaurantPizza(price=10, restaurant_id=r1.id, pizza_id=p1.id)
+  rp2 = RestaurantPizza(price=12, restaurant_id=r1.id, pizza_id=p2.id)
+
+  db.session.add_all([rp1, rp2])
+  db.session.commit()
