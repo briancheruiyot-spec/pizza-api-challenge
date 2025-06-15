d3aa24413301b3ea6d48ac93ae207ff3b07714ad
briancheruiyot-spec <briancheruiyot6@gmail.com>
2025-06-15 22:44:11 +0300

    Updated pizza.py

---
+++ server/models/pizza.py
@@ -0,0 +1,17 @@
+from .. import db
+
+class Pizza(db.Model):
+  __tablename__ = 'pizzas'
+
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(50), nullable=False)
+  ingredients = db.Column(db.String(255), nullable=False)
+  
+  restaurants = db.relationship('RestaurantPizza', back_populates='pizza')
+  
+  def to_dict(self):
+    return {
+      "id": self.id,
+      "name": self.name,
+      "ingredients": self.ingredients
+    }
